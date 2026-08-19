898b209946868d16ec36d69d33e77e27d0e405af
Cassandra Yen Pei Ying <cassandrayenpy@hlbb.hongleong.com.my>
2026-08-19 15:26:56 +0800

    Fixes: add eslint config

---
+++ .github/workflows/eslint.config.js
@@ -0,0 +1,11 @@
+// eslint.config.js
+module.exports = [
+    {
+        files: ["**/*.js", "**/*.ts"],
+        rules: {
+            "no-unused-wars": "warn",
+            "no-console": "warn",
+            "eqeqeq": "warn"
+        }
+    }
+];
