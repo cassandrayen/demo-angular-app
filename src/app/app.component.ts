b9dd50ef838b2bac9b434be2171399123d0d4fe7
Cassandra Yen Pei Ying <cassandrayenpy@hlbb.hongleong.com.my>
2026-08-21 17:21:08 +0800

    test eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title: number = 'demo-angular-app';
+
+  ngOnInit(): void {
+    // Leave this completely empty
+  }
+
+  testLintFailure() {
+    const myUselessVariable = "I am never used anywhere!";
+    // Do not `console.log` it or return it. Just leave it here.
+  }
 }
