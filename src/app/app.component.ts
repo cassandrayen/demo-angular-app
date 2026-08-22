64eaedc732db49568545f4116dfe93af17152c68
Cassandra Yen Pei Ying <cassandrayenpy@hlbb.hongleong.com.my>
2026-08-22 12:12:46 +0800

    fixed invalid data type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title: number = 'demo-angular-app';
+  title: string = 'demo-angular-app';
 }
