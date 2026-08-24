43ad1bb1429c3932c372ae08b3e538c783793ba4
Cassandra Yen Pei Ying <cassandrayenpy@hlbb.hongleong.com.my>
2026-08-24 08:48:48 +0800

    update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  public title = 'demo-angular-app';
-
-  // ESLint PASSES: It just sees a normal function returning a concatenated string.
-  // AI FAILS: It recognizes the context and flags a critical SQL injection risk.
-  public fetchUserData(userInput: string): string {
-    const dbQuery = "SELECT * FROM users WHERE username = '" + userInput + "'";
-    console.log("Executing Query:", dbQuery); // <-- Just add this small text
-    return dbQuery;
-  }
+  title = 'demo-angular-app';
 }
